afc25440abf1fc72fb754ca4d2b111d690b015c9
emran <shuvoorba28@gmail.com>
2023-09-01 08:57:20 +0600

    responsive category and cards for all devices

--- js/index.js
+++ js/index.js
@@ -21,16 +21,17 @@ const loadCards = async(categoryId) => {
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
     const data = await response.json();
     const cardContainer = document.getElementById('card-container');
+    cardContainer.textContent = '';
     data.data.forEach((card) => {
         const div = document.createElement('div');
         div.classList = 'card bg-base-100 shadow-xl';
         div.innerHTML = `
-        <figure><img class="rounded-b-2xl rounded-t-2xl" src="${card.thumbnail}" alt="User" /></figure>
+        <figure><img class="w-80 h-80 rounded-b-2xl rounded-t-2xl" src="${card.thumbnail}" alt="User" /></figure>
                 <div class="flex mt-5 flex-row">
                     <img class="rounded-full mr-5 h-12 w-12" src="assets/IMG_20220901_174230.jpg" alt="">
                     <div>
                         <h2 class="card-title">
-                            Shoes!
+                            ${card.title}
                         </h2>
                         <p class="inline">If a dog chews</p>
                         <img class="inline h-6" src="assets/icons8-verified-account-48.png" alt="">
